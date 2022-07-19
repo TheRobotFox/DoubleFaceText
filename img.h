bff9bd11e70b87ac96277fc2fbf73d7121e13b4a
NoHamster <pfoch.tomke@gmail.com>
2022-07-19 14:52:27 +0200

    img

---
+++ img.h
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+
+typedef struct Image* Image;
+
+Image Image_create();
+
+Image Image_load(const char *path);
+
+bool Image_from_file(Image img, const char *path);
+bool Image_from_color(Image img, int x, int y, unsigned char col);
+bool Image_from_text(const char *text);
+
+int Image_get_x(Image img);
+int Image_get_y(Image img);
+
+unsigned char *Image_get(Image img, int x, int y);
+
+bool Image_save(Image img, const char *path);
+void Image_free(Image img);
+
---
+++ img_internal.h
@@ -0,0 +1,6 @@
+
+struct Image
+{
+	int x,y;
+	unsigned char *data;
+};
